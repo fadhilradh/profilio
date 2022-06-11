12afba4b92e2449e3404d33bf9c98011f9df6f77
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2022-06-11 21:30:22 +0700

    add profile components

---
+++ app/Profile/Information.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Information = () => {
+  return (
+    <div className="rounded-lg border border-stroke-primary p-8">
+      Information
+    </div>
+  )
+}
+
+export default Information
--- app/Profile/index.tsx
+++ app/Profile/index.tsx
@@ -3,7 +3,33 @@ import React from 'react'
 import MainView from 'components/Layout/MainView'
 import Sidebar from 'components/Layout/Sidebar'
 
+import Career from './Career'
+import Education from './Education'
+import Gallery from './Gallery'
+import Information from './Information'
+
 const ProfileApp = () => {
+  const [activeTabIndex, setActiveTabIndex] = React.useState(0)
+
+  const tabsData = [
+    {
+      label: 'Information',
+      content: <Information />,
+    },
+    {
+      label: 'Career',
+      content: <Career />,
+    },
+    {
+      label: 'Education',
+      content: <Education />,
+    },
+    {
+      label: 'Gallery',
+      content: <Gallery />,
+    },
+  ]
+
   return (
     <div className="flex">
       <Sidebar />
@@ -26,6 +52,28 @@ const ProfileApp = () => {
               <h1 className="text-3xl text-primary">Wong Fei Hung</h1>
               <p className="text-[14px] text-secondary">Level 1 - #SG769891</p>
             </section>
+
+            <div className="mt-11 flex gap-x-8 border-b border-stroke-primary">
+              {tabsData.map((tab, idx) => {
+                return (
+                  <button
+                    key={idx}
+                    className={`border-b-2  py-2 px-4 transition-colors duration-300 ${
+                      idx === activeTabIndex
+                        ? 'border-accent text-primary'
+                        : 'border-transparent  text-tertiary hover:border-gray-200'
+                    }`}
+                    onClick={() => setActiveTabIndex(idx)}
+                  >
+                    {tab.label}
+                  </button>
+                )
+              })}
+            </div>
+
+            <div className="mt-16 mb-24">
+              <p>{tabsData[activeTabIndex].content}</p>
+            </div>
           </main>
         </>
       </MainView>
