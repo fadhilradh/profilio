f14addfc6ef4851394efa5985d29903fefdaf7ba
Fadhil Radhian <fadhilradhian@gmail.com>
2022-06-11 22:06:43 +0700

    fix responsive

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -5,7 +5,7 @@ import Logo from 'components/Logo'
 const Sidebar = () => {
   return (
     <>
-      <div className="w-full bg-surface-secondary py-10 md:h-screen md:w-4/12 md:py-24 xl:w-3/12">
+      <div className="w-full bg-surface-secondary py-10 md:h-screen md:w-5/12 md:py-24 xl:w-3/12">
         <Logo />
         <div className="mt-16 px-3 text-center md:pl-32 md:pr-10 md:text-left">
           <h1 className="mb-4 text-[32px] leading-9 text-primary">
